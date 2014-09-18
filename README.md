c344d3b55acfa6129d8ab1bd90b63e5830e782be
Daniel <ddtxra@gmail.com>
2014-09-18 16:58:14 +0200

    updating doc

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A sparql explorer ultra simple based on the original idea of [SNORQL](https://gi
 
 [AngularJS](http://angularjs.org) + [Brunch](http://brunch.io) + [Bootstrap](http://twitter.github.com/bootstrap/)
 
-The purpose of this project is to develop a new version of the original [SNORQL](https://github.com/kurtjx/SNORQL) interface that use the latest web standards for javascript and CSS. Live site http://evaletolab.github.io/angular-snorql
+The purpose of this project is to develop a new version of the original [SNORQL](https://github.com/kurtjx/SNORQL) interface that use the latest web standards for javascript and CSS. Live site http://calipho-sib.github.io/nextprot-snorql
 
 ## Installation
 ```
@@ -16,16 +16,16 @@ $ node app
 ```
 
 ##Sparql config: add your own SPARQL endpoint and examples
-You can provide SPARQL examples to the user interface by editing file [queries.json](app/assets/queries.json). 
-You can also set your own sparql endpoint by setting the variables (*namespacePrefixes* and *sparqlEndpoint*) in the file  [app.js](app/js/app.factory.js) 
+You can provide SPARQL examples to the user interface by editing file [queries.json](app/assets/queries.json).
+You can also set your own sparql endpoint by setting the variables (*namespacePrefixes* and *sparqlEndpoint*) in the file  [app.js](app/js/app.factory.js)
 
 ### deploy compiled application on github pages
 ```
 #
-# change your project base directory in the script 
+# change your project base directory in the script
 sh build-gh-pages.sh [/base/ (default angular-snorql)] [remote (default origin)]
 ```
- 
+
 ### minimize the project for production
 ```
 $ ./node_modules/.bin/brunch build -m
--- app/js/app.config.js
+++ app/js/app.config.js
@@ -3,26 +3,6 @@
 angular.module('snorql.config', []).factory('config', [
     function () {
 
-
-        var namespacePrefixes={
-             owl:'http://www.w3.org/2002/07/owl#',
-             xsd:'http://www.w3.org/2001/XMLSchema#',
-             rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
-             rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
-             foaf: 'http://xmlns.com/foaf/0.1/',
-             geo:'http://www.w3.org/2003/01/geo/wgs84_pos#',
-             dc: 'http://purl.org/dc/elements/1.1/',
-             '': 'http://dbpedia.org/resource/',
-             dbpedia2: 'http://dbpedia.org/property/',
-             dbpedia: 'http://dbpedia.org/',
-             skos: 'http://www.w3.org/2004/02/skos/core#',
-             category: 'http://dbpedia.org/resource/Category:',
-             dcterms: 'http://purl.org/dc/terms/',
-             ontology: 'http://dbpedia.org/ontology/',
-             virtuoso:'http://www.openlinksw.com/virtrdf-data-formats'
-        };
-
-        /*
         var namespacePrefixes={
             rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
             rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
@@ -47,12 +27,12 @@ angular.module('snorql.config', []).factory('config', [
             db: 'http://nextprot.org/rdf/db/',
             context: 'http://nextprot.org/rdf/context/'
         };
-        */
-        
+
+
         // global application configuration
         var defaultConfig = {
             sparql : {
-              endpoint: 'http://dbpedia.org/sparql',
+              endpoint: 'http://localhost:8080/nextprot-api/sparql',
               examples: 'queries.json',
               prefixes: namespacePrefixes
           }
