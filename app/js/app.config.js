2cf8988b86a1e19ab97571916ab4f836127a74e3
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2014-11-06 17:57:13 +0100

    fixed bug in new prefix....

--- app/js/app.config.js
+++ app/js/app.config.js
@@ -21,7 +21,7 @@ angular.module('snorql.config', []).factory('config', [
             evidence: 'http://nextprot.org/rdf/evidence/',
             xref: 'http://nextprot.org/rdf/xref/',
             publication: 'http://nextprot.org/rdf/publication/',
-            identifier: <http://nextprot.org/rdf/identifier/>,
+            identifier: 'http://nextprot.org/rdf/identifier/',
             term: 'http://nextprot.org/rdf/terminology/',
             gene: 'http://nextprot.org/rdf/gene/',
             source: 'http://nextprot.org/rdf/source/',
