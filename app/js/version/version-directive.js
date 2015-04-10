4547dc03429967b3c80e69610a258ef192303c72
fnikitin <frederic.nikitin@gmail.com>
2015-04-10 15:44:01 +0200

    Do not display build if it has not been replaced by a number

--- app/js/version/version-directive.js
+++ app/js/version/version-directive.js
@@ -13,4 +13,16 @@ angular.module('snorql.version.version-directive', [])
   return function (scope, elm, attrs) {
     elm.text(build);
   };
+}])
+.directive('sqBuildVersion', ['version', 'build', function (version, build) {
+
+  return function (scope, elm, attrs) {
+
+    var content = version;
+
+    if (!isNaN(build))
+      content += " (build "+build+")";
+
+    elm.text(content);
+  };
 }]);
