237734b40c2aa167928f9d4b76775c15337feaaa
fnikitin <frederic.nikitin@gmail.com>
2015-04-14 11:05:07 +0200

    Remove unused code

--- app/js/version/version-directive.js
+++ app/js/version/version-directive.js
@@ -2,18 +2,6 @@
 
 angular.module('snorql.version.version-directive', [])
 
-.directive('sqVersion', ['version', function(version) {
-
-  return function(scope, elm, attrs) {
-    elm.text(version);
-  };
-}])
-.directive('sqBuild', ['build', function (build) {
-
-  return function (scope, elm, attrs) {
-    elm.text(build);
-  };
-}])
 .directive('sqBuildVersion', ['version', 'build', function (version, build) {
 
   return function (scope, elm, attrs) {
--- app/js/version/version.js
+++ app/js/version/version.js
@@ -1,7 +1,6 @@
 'use strict';
 
 angular.module('snorql.version', [
-  'snorql.version.interpolate-filter',
   'snorql.version.version-directive'
 ])
 
