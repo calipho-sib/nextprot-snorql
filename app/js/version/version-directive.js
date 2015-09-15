029ea60b2f059625329f6b8e9631557711df81e6
fnikitin <frederic.nikitin@gmail.com>
2015-09-15 10:44:57 +0200

    Add git hash in build version info

--- app/js/version/version-directive.js
+++ app/js/version/version-directive.js
@@ -2,15 +2,14 @@
 
 angular.module('snorql.version.version-directive', [])
 
-.directive('sqBuildVersion', ['version', 'build', function (version, build) {
+.directive('sqBuildVersion', ['version', 'build', 'githash', function (version, build, githash) {
 
   return function (scope, elm, attrs) {
 
     var content = version;
 
     if (!isNaN(build))
-      content += " (build "+build+")";
-
+      content += " (build "+build+"#"+githash+")";
     elm.text(content);
   };
 }]);
--- app/js/version/version.js
+++ app/js/version/version.js
@@ -3,6 +3,6 @@
 angular.module('snorql.version', [
   'snorql.version.version-directive'
 ])
-
-.value('version', '0.1.5')
-.value('build', 'NX_BUILD');
+    .value('version', '0.1.5')
+    .value('build', 'NX_BUILD')
+    .value('githash', 'GIT_HASH');
