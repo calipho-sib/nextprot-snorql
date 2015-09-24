81682185c03c8bd576f22a9589a0f0ef860115bd
fnikitin <frederic.nikitin@gmail.com>
2015-09-24 16:29:26 +0200

    new release v0.3.0

--- app/js/version/version.js
+++ app/js/version/version.js
@@ -4,7 +4,7 @@ angular.module('snorql.version', [
   'snorql.version.version-directive'
 ])
     .constant('RELEASE_INFOS', {
-        'version': '0.1.5',
+        'version': '0.3.0',
         "isProduction": 'IS_PRODUCTION', // i.e 'true'
         'build': 'BUILD_NUMBER', // '926'
         'githash': 'GIT_HASH' // 'e3a1a30'
