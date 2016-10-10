17b23f75e7905ae17549c6f6f244348d248dcb47
fnikitin <frederic.nikitin@gmail.com>
2016-10-10 13:01:41 +0200

    Prepare next dev version

--- app/js/version/version.js
+++ app/js/version/version.js
@@ -4,7 +4,7 @@ angular.module('snorql.version', [
   'snorql.version.version-directive'
 ])
     .constant('RELEASE_INFOS', {
-        'version': '2.0.0',
+        'version': '2.1.0',
         "isProduction": 'IS_PRODUCTION', // i.e 'true'
         'build': 'BUILD_NUMBER', // '926'
         'githash': 'GIT_HASH' // 'e3a1a30'
