21c6d9d12fb32ab8c1249774205bea6994cb1934
fnikitin <frederic.nikitin@isb-sib.ch>
2018-01-05 13:51:40 +0100

    Adding branch name info in footer

--- app/js/version/version.js
+++ app/js/version/version.js
@@ -3,6 +3,7 @@
 angular.module('snorql.version', [])
     .constant('RELEASE_INFOS', {
         'version': '2.5.0',
+        'branch': 'BRANCH_NAME',
         "isProduction": 'IS_PRODUCTION', // i.e 'true'
         'build': 'BUILD_NUMBER', // '926'
         'githash': 'GIT_HASH' // 'e3a1a30'
