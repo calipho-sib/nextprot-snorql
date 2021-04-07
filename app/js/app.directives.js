539bbb98f494ee084ab577ec73be5cba80761164
Pierre-Andre Michel <pierre-andre.michel@isb-sib.ch>
2021-04-07 18:18:09 +0200

    moved api calls to https for all platforms (incl. BUILD)

--- app/js/app.directives.js
+++ app/js/app.directives.js
@@ -60,9 +60,9 @@ function getNeXtProtUrl(config) {
         }
     }
     else if(input === "sparql") {
-        return "http://" + config.environment + "-api.nextprot.org/sparql";
+        return "https://" + config.environment + "-api.nextprot.org/sparql";
     }
-    else return "http://" + config.environment + "-" + input + ".nextprot.org";
+    else return "https://" + config.environment + "-" + input + ".nextprot.org";
 }};
 
 function getNxqId() {
